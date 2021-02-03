f4c61577ac71ba8a41f4270f6c87100965252e9c
jeanpierreba <2713@holbertonschool.com>
2021-02-03 20:12:28 +0000

    size of variants

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - print the size of each variable
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of a char: %zu bytes\n", sizeof(charType));
+	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longType));
+	printf("Size of a float: %zu byte\n", sizeof(floatType));
+
+	return (0);
+}
