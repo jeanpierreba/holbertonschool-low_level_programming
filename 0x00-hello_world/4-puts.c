a363ce331e248e0a936dff0fa94312f2dcd9cc4b
jeanpierreba <2713@holbertonschool.com>
2021-02-03 19:57:41 +0000

    added description of main function and return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 
-int main(void)
+/**
+ * main - prints:'"Programming is like building a multilingual puzzle'
+ * Return: 0
+ */
 
+int main(void)
 {
 	char text1[] = "\"Programming is like building a multilingual puzzle";
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+/**
+ * main - print "With proper grammar, but the..."
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
