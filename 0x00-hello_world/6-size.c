0574ab335977ccaf27c43f662be103efedaff7cf
jeanpierreba <2713@holbertonschool.com>
2021-02-03 20:22:01 +0000

    size of variants

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of a char: %zu bytes\n", sizeof(charType));
 	printf("Size of an int: %zu bytes\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes\n", sizeof(longType));
+	printf("Size of a long int: %zu bytes\n", sizeof(doubleType));
 	printf("Size of a float: %zu byte\n", sizeof(floatType));
 
 	return (0);
