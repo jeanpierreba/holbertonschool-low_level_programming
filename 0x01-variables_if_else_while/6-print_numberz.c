aabc33bf69018c467ebc72a9a1428431fe03ab3e
jeanpierreba <2713@holbertonschool.com>
2021-02-04 21:33:54 +0000

    added loops of alphabet and numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print numbers from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '48'; n < '58'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n')
+		return (0);
+
+}
