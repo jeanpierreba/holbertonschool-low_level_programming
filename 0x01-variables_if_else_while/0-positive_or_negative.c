3ebacd9568f0101aaae137ceb93bace5d535c6b7
jeanpierreba <2713@holbertonschool.com>
2021-02-04 18:03:55 +0000

    fixed several errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,8 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
-       		printf("%d is negative", n);
-	else	       
+		printf("%d is negative", n);
+	else
 		printf("%d is zero", n);
 	return (0);
 }
