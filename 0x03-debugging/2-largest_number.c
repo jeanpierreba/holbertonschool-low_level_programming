2ed2c8fdab1718690409c229206b57ae4053634e
jeanpierreba <2713@holbertonschool.com>
2021-02-10 00:55:26 +0000

    code fixed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	if (b > a && b > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
-	}	
-	if (c > a && c > b)
+	}
+	else
 	{
 		largest = c;
 	}
-	    return (largest);
+
+	return (largest);
 }
