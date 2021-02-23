a9dc6390715dcc9206287df911c08b28b3b30f55
jeanpierreba <2713@holbertonschool.com>
2021-02-23 00:00:30 +0000

    replaced while with for

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,16 +9,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = 0;
-
-	while (i < 8)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
 		_putchar('\0');
-		i++;
 	}
 }
