6f7d85d141d9dce07c2cb106e5b25cdb2425fa68
jeanpierreba <2713@holbertonschool.com>
2021-02-24 20:06:04 +0000

    strings and equations with recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * factorial - calculate the factorial of a given number
+ * @n: number
+ * Return: factorial of the number
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
