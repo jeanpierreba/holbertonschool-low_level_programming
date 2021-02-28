6b54fd0249822091ef97080022a87410ec647a91
jeanpierreba <2713@holbertonschool.com>
2021-02-28 16:04:12 +0000

    added description of main function

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - print the number of arguments
+ * @argc: number of arguments
+ * @argv: pointer to a pointer to a list of
+ * command lines arguments
+ * Return: 0
+ */
+
 int main(int argc, char **argv)
 {
 	(void) argv;
