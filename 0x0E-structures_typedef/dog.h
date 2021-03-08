2538b4da53daf02b5cc089d44f85d012a82b64fd
jeanpierreba <2713@holbertonschool.com>
2021-03-08 20:55:57 +0000

    FIX of header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,18 +8,12 @@
  * @owner: owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - typedef of struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
