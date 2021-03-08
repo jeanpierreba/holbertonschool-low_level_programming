033fa6239b6dca4f28aa5ef959c1346ebb0114f4
jeanpierreba <2713@holbertonschool.com>
2021-03-08 20:33:01 +0000

    added header and README

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef dogH
+#define dogH
+
+/**
+ * struct dog - dog's info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef of struct dog
+ */
+
+typedef struct dog dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
