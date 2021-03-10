bb832621e6c622c2c60eac0f9416465b5b41fe2b
jeanpierreba <2713@holbertonschool.com>
2021-03-10 17:38:55 +0000

    fixed header and print name file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
-#ifndef FUNCTIONP_H
-#define FUNCTIONP_H
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif //*FUNCTION_H*//
