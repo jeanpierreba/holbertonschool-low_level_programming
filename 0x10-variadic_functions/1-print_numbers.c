dfe987ee7e69e6aca8f1b285dd82b98a0e179b00
jeanpierreba <2713@holbertonschool.com>
2021-03-11 17:09:42 +0000

    Added if statement inside the loop

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,14 +11,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
-	if (separator == NULL)
-		separator = "";
-
 	va_start(numbers, n);
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(numbers, int), separator);
-	printf("%d\n", va_arg(numbers, int));
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			printf("%i", va_arg(numbers, int));
+		else if (separator != NULL && i == 0)
+			printf("%i", va_arg(numbers, int));
+		else
+			printf("%s%i", separator, va_arg(numbers, int));
+	}
 
+	printf("\n");
 	va_end(numbers);
 }
