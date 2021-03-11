8e6aa5fc31701fe9055765162fd1bae64d906847
jeanpierreba <2713@holbertonschool.com>
2021-03-11 17:14:48 +0000

    fixed line 24 with printf

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			str = "(nil)";
 		if (separator == NULL)
-			separator = "";
+			printf("%s", str);
 		else if (separator != NULL && i == 0)
 			printf("%s", str);
 		else
