b2e422deecaf9218e6203416dfddf952bdbfd4d7
jeanpierreba <2713@holbertonschool.com>
2021-03-23 16:44:18 +0000

    File that return the nth number of the linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index -  returns the nth node
+ * @head: pointer to the first element
+ * @index: index to find in list
+ * Return: nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int counter;
+
+	if (head == NULL)
+		return (0);
+
+	for (counter = 0; count < index; counter++)
+	{
+		if (head == NULL)
+			return (0);
+		head = head->next;
+	}
+	return (head);
+}
