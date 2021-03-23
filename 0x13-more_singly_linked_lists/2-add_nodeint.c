77265d3d95d76c0f2081d221b917cb46146c6b2c
jeanpierreba <2713@holbertonschool.com>
2021-03-23 15:55:48 +0000

    File to add new node at beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,5 +2,16 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	
+	listint_t *newn;
+
+	newn = malloc(sizeof(listint_t));
+
+	if (newn == NULL)
+		return (NULL);
+
+	newn->n = n;
+	newn->next = *head;
+	*head = newn;
+
+	return (newn);
 }
