159104b210e8cc64b79b42fd108e8252d9590f84
jeanpierreba <2713@holbertonschool.com>
2021-03-25 20:46:30 +0000

    Added file to print the value of a bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* get_bit - get the value of a bit at a given index
+* @n: number in base10
+* @index: index
+* Return: value of the bit
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	bit = (n >> index) & 1;
+
+	return (bit);
+}
